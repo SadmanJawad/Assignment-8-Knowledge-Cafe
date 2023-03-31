051ccea6597bd842514429b16222e9945346c783
SadmanJawad <jawadnabil977@gmail.com>
2023-03-31 15:24:57 +0600

    empty project, tail-daisy uninstall

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
-import Header from '../fakeData/components/Header/Header'
 import './App.css'
 
 function App() {
 
-
   return (
     <div className="App">
-      
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    
-      <Header></Header>
+
     </div>
   )
 }
