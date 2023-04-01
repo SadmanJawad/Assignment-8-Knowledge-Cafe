51d9e91b9562cbce2a796429853467907b4dfc30
SadmanJawad <jawadnabil977@gmail.com>
2023-04-01 22:59:43 +0600

    final push

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Header.css'
+import logo from "../images/person.jpg"
 const Header = () => {
     return (
         <div className='header'>
                 <h1>Knowledge Cafe</h1>
-                <img className='navPersonPic' src="/images/person.jpg" alt="" />
+                 {/* <img className='navPersonPic' src="./images/person.jpg" alt="" /> */}
+                <img className='navPersonPic' src={logo} alt="" />
         </div>
     );
 };
